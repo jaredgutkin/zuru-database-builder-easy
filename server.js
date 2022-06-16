0f713526a201adf79c21a734c4b1df2e085e758f
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-15 21:28:27 -0500

    crud methods

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8005
 require('dotenv').config()
 
 let db,
-    dbConnectionStr = process.env.DB_STRING
+    dbConnectionStr = process.env.DB_STRING,
     dbName = 'StarTrekAPI'
 
 MongoClient.connect(dbConnectionStr)
@@ -13,6 +13,29 @@ MongoClient.connect(dbConnectionStr)
         console.log(`connected to ${dbName} Database`)
         db = client.db(dbName)
     })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get('/', (req, res) =>{
+    let contents = db.collection('Alien Info').find().toArray()
+
+    console.log(contents)
+})
+
+app.post('/api', (req, res)=>{
+
+})
+
+app.put('/updateEntry', (req, res)=>{
+
+})
+
+app.delete('/deleteEntry', (req, res)=>{
+    
+})
 
 app.listen(process.env.PORT || PORT, () =>{
     console.log(`server is running on port ${PORT}`)
